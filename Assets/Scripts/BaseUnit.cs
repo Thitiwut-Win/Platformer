96093244b00e60ead928eed67278826e4e3d71f6
Thitiwut-Win <bmwin9825@gmail.com>
2025-05-27 17:29:07 +0700

    Platforms, Jump and Walls

--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -6,6 +6,7 @@ public class BaseUnit : MonoBehaviour
     public float maxHealth;
     public float damage;
     public float speed;
+    public float jumpSpeed;
     protected Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,96 @@ using UnityEngine;
 
 public class Player : BaseUnit
 {
+    private float horizontalInput;
+    [SerializeField]
+    private Animator animator;
+    private bool isFacingRight = true;
+    private bool isJumping = false;
+    private bool isGrounded = true;
+    private bool isWallSliding = false;
+    private bool isWallSticking = false;
+    private bool isWallJumping = false;
+    [Header("GroundCheck")]
     [SerializeField]
     private LayerMask isGround;
     [SerializeField]
     private Transform groundCheck;
-    private bool isGrounded = true;
+    [SerializeField]
+    private Vector2 groundCheckSize;
+    [Header("WallCheck")]
+    [SerializeField]
+    private LayerMask isWall;
+    [SerializeField]
+    private Transform wallCheck;
+    [SerializeField]
+    private Vector2 wallCheckSize;
     void Update()
     {
-        isGrounded = false;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.05f, isGround);
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject != gameObject) isGrounded = true;
-        }
+        horizontalInput = Input.GetAxisRaw("Horizontal");
         Move();
+        SetAnimatorParameter();
     }
     void Move()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 v = rb.linearVelocity;
-        v.x =  horizontalInput * speed;
-        if (Input.GetKey(KeyCode.Space) && isGrounded) v.y = 4;
+        if (!isWallJumping) v.x = horizontalInput * speed;
+        if (GroundCheck())
+        {
+            isGrounded = true;
+            isJumping = false;
+            isWallJumping = false;
+        }
+        else isGrounded = false;
+        if (WallCheck() && !isGrounded && v.x != 0)
+        {
+            isWallSliding = true;
+            isWallJumping = false;
+        }
+        else isWallSliding = false;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print(isGrounded);
+            if (isGrounded)
+            {
+                isJumping = true;
+                v.y = jumpSpeed;
+            }
+            else if (isWallSliding)
+            {
+                isWallJumping = true;
+                v.y = jumpSpeed;
+                float jumpDirection = -transform.localScale.x / 4;
+                v.x = jumpDirection * speed;
+            }
+        }
+        Flip(v);
         rb.linearVelocity = v;
+    }
+    private void Flip(Vector3 v)
+    {
+        if(v.x > 0) transform.localScale = new Vector3(4, 4, 4);
+        else if(v.x < 0) transform.localScale = new Vector3(-4, 4, 4);
+    }
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, isGround);
+    }
+    private bool WallCheck()
+    {
+        return Physics2D.OverlapBox(wallCheck.position, wallCheckSize, 0, isWall);
+    }
+    private void SetAnimatorParameter()
+    {
+        animator.SetFloat("SpeedX", Mathf.Abs(horizontalInput));
+        animator.SetFloat("SpeedY", rb.linearVelocityY);
+        animator.SetBool("IsJumping", isJumping);
+        animator.SetBool("IsWallSticking", isWallSticking);
+        animator.SetBool("IsWallJumping", isWallJumping);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(groundCheck.position, groundCheckSize);
+        Gizmos.DrawCube(wallCheck.position, wallCheckSize);
     }
 }
